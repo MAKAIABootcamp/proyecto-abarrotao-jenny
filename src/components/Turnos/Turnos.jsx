b23bdac2ee47d3059199776fec3f943baea24ae1
jennyvanessaquinto <vane.quinto19@gmail.com>
2022-12-17 23:41:51 -0500

    Get turnos and display it

--- src/components/Turnos/Turnos.jsx
+++ src/components/Turnos/Turnos.jsx
@@ -1,12 +1,29 @@
+import { doc, getDoc } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
+import { dataBase } from '../../firebase/firebaseConfig';
+import { Badge, Card } from "react-bootstrap";
+
 
 const Turnos = () => {
+  const { turnos } = useSelector((store) => store.turnoStore);
   const dispatch = useDispatch();
   const [location, setLocation] = useState(false)
-  const navigate = useNavigate();
+  const [tienda, setTienda] = useState({})
+
+  const getTurnoInfo = async (uid) => {
+    try {
+      const docRef = doc(dataBase, "turnos", uid)
+      const docu = await getDoc(docRef)
+      const dataFinal = docu.data()
+      setTienda( dataFinal )
+      return tienda
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   useEffect(() => {
 
@@ -37,9 +54,23 @@ const Turnos = () => {
 
   }, [dispatch])
   return (    
-    <div>
+    <div >
       {
-        location?<div>Turnos</div>:(navigate('/home'))
+        location ? (
+          turnos.map((turno, index) => ((
+            
+              <section key={index} >                  
+                  <Card style={{ width: '18rem', height: '50%' }} >
+                      <Card.Img variant="top" src='' style={{ height: '10rem', objectFit: 'cover' }} className='imgCard'  />
+                      <Badge bg="warning" text="dark"></Badge>
+                      <Card.Body>
+                          <Card.Title>{`Horario ${turno.hour}:${turno.minutes}`}</Card.Title>
+                          <Card.Text>{`${turno.list}`}</Card.Text>
+                      </Card.Body>
+                  </Card>
+              </section>)
+          ))
+      ) : (<></>)
       }
     </div>
   )
